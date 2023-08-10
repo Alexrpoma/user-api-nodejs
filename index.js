732dd4128f94c5900e569988f9c78b1533c32347
Alex Poma <alex.rpoma@outlook.com>
2023-08-10 18:28:30 -0400

    Dockerfile added

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ app.get('/user', (req, res) => {
 })
 
 app.listen(port, () => console.log('Listening on port ', port))
+
+//Docker build image example:
+//docker build --tag user-api-demo:1.0.0 .
+
+//Docker run dockerfile example:
+//docker run --name user-api -d -p 3000:5000 user-api-demo:1.0.0
+//the public port is 3000 and the port where the express server is listening is 5000
